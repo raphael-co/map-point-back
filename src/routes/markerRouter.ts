ee5b819b0894feacf65798e6c35273b6ad94ab41
raphael-co <76869389+raphael-co@users.noreply.github.com>
2024-07-31 16:53:02 +0200

    test addMarker

--- src/routes/markerRouter.ts
+++ src/routes/markerRouter.ts
@@ -2,10 +2,10 @@ import { Router } from 'express';
 
 import { authenticateToken } from '../middleweares/authMiddleweares';
 import { validateCreateMarker } from '../middleweares/markerMiddlewares';
-import { createMarker } from '../controllers/markerController';
+import { createMarker, getAllMarkers } from '../controllers/markerController';
 
 const markerRouter = Router();
 
 markerRouter.post('/create', authenticateToken, validateCreateMarker, createMarker);
-
+markerRouter.get('/', authenticateToken, getAllMarkers);
 export default markerRouter;
