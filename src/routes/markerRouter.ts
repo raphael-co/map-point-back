bbe5e1c83faf05ed2b06b88070cedd0a701aafec
raphael-co <76869389+raphael-co@users.noreply.github.com>
2024-08-01 12:34:57 +0200

    label raiting personalisé en fontion du type

--- src/routes/markerRouter.ts
+++ src/routes/markerRouter.ts
@@ -21,31 +21,25 @@ markerRouter.post('/addLabels', authenticateToken, async (req: Request, res: Res
 
     try {
         const connection = await pool.getConnection();
-
-        // Commence une transaction
         await connection.beginTransaction();
 
         try {
             for (const label of labels) {
-                // Vérifier si le label existe déjà pour ce markerType
                 const [existingLabel] = await connection.query<RowDataPacket[]>(
                     'SELECT id FROM RatingLabels WHERE marker_type = ? AND label = ?',
                     [markerType, label]
                 );
 
-                // Ajouter le label seulement s'il n'existe pas déjà
                 if (existingLabel.length === 0) {
                     await connection.query('INSERT INTO RatingLabels (marker_type, label) VALUES (?, ?)', [markerType, label]);
                 }
             }
 
-            // Valide la transaction
             await connection.commit();
             connection.release();
             res.status(201).json({ status: 'success', message: 'Labels added successfully.' });
 
         } catch (err) {
-            // En cas d'erreur, annule la transaction
             await connection.rollback();
             connection.release();
             console.error('Error adding labels:', err);
@@ -57,5 +51,34 @@ markerRouter.post('/addLabels', authenticateToken, async (req: Request, res: Res
     }
 });
 
+// Route pour récupérer les labels en fonction du type de marqueur
+markerRouter.get('/labels/:markerType', authenticateToken, async (req: Request, res: Response) => {
+    const { markerType } = req.params;
+
+    try {
+        const connection = await pool.getConnection();
+        try {
+            const [labels] = await connection.query<RowDataPacket[]>(
+                'SELECT label FROM RatingLabels WHERE marker_type = ?',
+                [markerType]
+            );
+
+            if (labels.length === 0) {
+                connection.release();
+                return res.status(404).json({ status: 'error', message: `No labels found for marker type ${markerType}` });
+            }
+
+            connection.release();
+            res.status(200).json({ status: 'success', data: labels });
+        } catch (err) {
+            connection.release();
+            console.error('Error fetching labels:', err);
+            res.status(500).json({ status: 'error', message: 'Internal server error' });
+        }
+    } catch (error) {
+        console.error('Database connection error:', error);
+        res.status(500).json({ status: 'error', message: 'Internal server error' });
+    }
+});
 
 export default markerRouter;
