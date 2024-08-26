a6b9db9d9078e13cef10d39c531e6324d2a5d45b
raphael-co <76869389+raphael-co@users.noreply.github.com>
2024-08-26 18:38:09 +0200

    notification a tester

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,12 @@ import cors from 'cors';
 import routes from './routes/routes';
 import { initializeDatabase } from './utils/config/databaseInit';
 import { setSocketServer } from './controllers/setSocketServer';
+import jwt, { JwtPayload } from 'jsonwebtoken'; // Importation de JwtPayload pour les types
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const app = express();
 const port = 3000;
@@ -41,6 +47,35 @@ setSocketServer(io);
 // Set up Socket.IO connection
 io.on('connection', (socket) => {
     console.log(`New client connected: ${socket.id}`);
+
+    // Récupérer le token depuis la requête de connexion
+    const token = socket.handshake.query.token;
+
+    if (token && typeof token === 'string' && SECRET_KEY) {
+        try {
+            // Vérifier et décoder le token
+            const decoded = jwt.verify(token, SECRET_KEY); 
+
+            // Assurez-vous que le token contient l'ID utilisateur et est de type JwtPayload
+            if (typeof decoded !== 'string' && 'id' in decoded) {
+                const userId = (decoded as JwtPayload).id; // Utilisation de type assertion pour JwtPayload
+                // Ajouter le socket à une salle spécifique à l'utilisateur
+                socket.join(`user_${userId}`);
+                console.log(`User ${userId} joined room user_${userId}`);
+
+                // Écouter d'autres événements si nécessaire
+            } else {
+                console.error('Token does not contain user ID');
+                socket.disconnect();
+            }
+        } catch (err) {
+            console.error('Token validation failed:', err);
+            socket.disconnect(); // Déconnecter le socket si la validation échoue
+        }
+    } else {
+        console.error('No valid token provided or token is not a string');
+        socket.disconnect(); // Déconnecter le socket si aucun token n'est fourni ou si le token n'est pas valide
+    }
 
     socket.on('message', (data) => {
         console.log(`Message from client: ${data}`);
