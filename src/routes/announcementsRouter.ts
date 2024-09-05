e658bd3a485e7e138c727233586148fbd3f06af7
raphael-co <76869389+raphael-co@users.noreply.github.com>
2024-09-05 17:57:37 +0200

    anoucement ok

--- src/routes/announcementsRouter.ts
+++ src/routes/announcementsRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { authenticateTokenAdmin } from '../middleweares/authMiddleweares';
+import { authenticateToken, authenticateTokenAdmin } from '../middleweares/authMiddleweares';
 import { addAnnouncement, deleteAnnouncement, getAnnouncementById, getAnnouncements, updateAnnouncement } from '../controllers/announcementController';
 import { validateAnnouncement } from '../middleweares/announcementsMiddleweares';
 
@@ -10,8 +10,8 @@ const announcementsRouter = Router();
 
 // Routes pour les annonces
 announcementsRouter.post('/add', authenticateTokenAdmin,validateAnnouncement, addAnnouncement);
-announcementsRouter.get('/', authenticateTokenAdmin, getAnnouncements);
-announcementsRouter.get('/:id', authenticateTokenAdmin, getAnnouncementById);  // Nouvelle route pour obtenir une annonce par ID
+announcementsRouter.get('/', authenticateToken, getAnnouncements);
+announcementsRouter.get('/:id', authenticateToken, getAnnouncementById);  // Nouvelle route pour obtenir une annonce par ID
 announcementsRouter.put('/update/:id', authenticateTokenAdmin, updateAnnouncement);
 announcementsRouter.delete('/:id', authenticateTokenAdmin, deleteAnnouncement);
 
