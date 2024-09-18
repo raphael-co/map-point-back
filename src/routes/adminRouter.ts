0464c76b41978dc01c8ed0b9fa332634cbc4f681
raphael-co <76869389+raphael-co@users.noreply.github.com>
2024-09-18 16:49:11 +0200

    test blocked fonction

--- src/routes/adminRouter.ts
+++ src/routes/adminRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { authenticateTokenAdmin } from '../middleweares/authMiddleweares';
-import { getAllMarkersAdmin } from '../controllers/adminController';
+import { getAllMarkersAdmin, updateMarkerBlockedStatus } from '../controllers/adminController';
 
 const adminRouter = Router();
 
 // Routes pour les annonces
 adminRouter.get('/markers', authenticateTokenAdmin, getAllMarkersAdmin);
+adminRouter.patch('/markers/blocked', authenticateTokenAdmin, updateMarkerBlockedStatus);
 
 export default adminRouter;
